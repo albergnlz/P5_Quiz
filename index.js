f7d141ce8052c04544227b3c89d54de6e97bd111
Alberto González Iglesias <albertogonzaleziglesias.97@gmail.com>
2018-05-03 11:17:34 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+const Sequelize = require("sequelize");
+const {models}=require('../models');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET CREDITS. */
+router.get('/credits', function(req, res, next) {
+    res.render('credits', { title: 'ALBERTO' });
+});
+
+/* GET Quizzes. */
+router.get('/quizzes', function(req, res, next) {
+    models.quiz.findAll()
+        .then(quizzes =>{
+            res.render('quizzes',{quizzes});
+        })
+        .catch(error=> next(error))
+});
+
+module.exports = router;
